8a92c8e9d88ea3e4e60fce7bd8386c1909627abf
Alexis Loyola <alexis.loyola@gmail.com>
2019-07-13 22:55:24 -0400

    Redifinición de estructura de directorio

--- faq.php
+++ faq.php
@@ -2,19 +2,19 @@
 <!DOCTYPE html>
 <html lang="zxx">
     <?php
-    require_once('head.php');
+    require_once('content/head.php');
     ?>
     <body class="shop home-page">
-    <?php    require_once('sideNavMenu.php'); ?>
+    <?php    require_once('content/sideNavMenu.php'); ?>
     <div class="wrapper reveal-side-navigation">
 		<div class="wrapper-inner">
             <?php 
-            require_once('header.php');
-            require_once('content-faq.php');
-            require_once('footer.php');
+            require_once('content/header.php');
+            require_once('content/content-faq.php');
+            require_once('content/footer.php');
             ?>
     	</div>
 	</div>
-    <?php    require_once('scripts.php'); ?>
+    <?php    require_once('content/scripts.php'); ?>
     </body>
 </html>
